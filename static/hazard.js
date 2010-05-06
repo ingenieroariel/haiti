79dcec1210c6819b93d9a8ea7514f0a61b391829
Ivan Willig <iwillig@gmail.com>
2010-05-06 16:04:01 -0400

    added some ext stuff to the PDF page

--- static/hazard.js
+++ static/hazard.js
@@ -1,6 +1,6 @@
 
 function loadMap() { 
-  map = new OpenLayers.Map('hazard-map');
+  map = new OpenLayers.Map('map');
   layer = new OpenLayers.Layer.OSM("Simple OSM Map");
   map.addLayer(layer);
   map.setCenter(new OpenLayers.LonLat(-8051914,2104311),10)
--- views.py
+++ views.py
@@ -4,4 +4,11 @@ def haiti_index(request):
     return render_to_response("haiti-index.html")
 
 def data_search(request):
-    return render_to_response("data_search.html")
+    if request.method == 'GET':
+        try: 
+            query = request.GET["query"]
+        except: 
+            query = "Hazard"
+    if request.method == 'POST':
+        query = request.POST["query"]
+    return render_to_response("data_search.html",{"query": query})
