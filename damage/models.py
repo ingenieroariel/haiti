9bd7c5594af4a9108b7ed4f52e22a3a305e807c1
Ivan Willig <iwillig@gmail.com>
2010-05-26 13:15:51 -0400

    started on reporting application

--- damage/models.py
+++ damage/models.py
@@ -1,6 +1,5 @@
 from django.contrib.gis.db import models
-
-# Create your models here.
+from django.contrib import admin
 
 
 
@@ -14,3 +13,77 @@ class DamageMap(models.Model):
     description = models.CharField(max_length=10000)
     point = models.PointField()
     objects = models.GeoManager()
+
+
+class DamageReports(models.Model):
+    title = models.CharField(max_length=100) # the world bank has long title names
+    summary = models.TextField()
+    File = models.FileField(upload_to="static/pdfs/")
+    
+
+    def __unicode__(self):
+        return u"%s" % self.title
+
+class DamageReportAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(DamageReports,DamageReportAdmin)
+
+
+class HaitiAdmin(models.Model):
+    gid = models.IntegerField(primary_key=True)
+    objectid = models.IntegerField()
+    uid = models.IntegerField()
+    id_0 = models.IntegerField()
+    iso = models.CharField(max_length=3)
+    name_0 = models.CharField(max_length=75)
+    id_1 = models.IntegerField()
+    name_1 = models.CharField(max_length=75)
+    varname_1 = models.CharField(max_length=150)
+    nl_name_1 = models.CharField(max_length=50)
+    hasc_1 = models.CharField(max_length=15)
+    cc_1 = models.CharField(max_length=15)
+    type_1 = models.CharField(max_length=50)
+    engtype_1 = models.CharField(max_length=50)
+    validfr_1 = models.CharField(max_length=25)
+    validto_1 = models.CharField(max_length=25)
+    remarks_1 = models.CharField(max_length=125)
+    id_2 = models.IntegerField()
+    name_2 = models.CharField(max_length=75)
+    varname_2 = models.CharField(max_length=150)
+    nl_name_2 = models.CharField(max_length=75)
+    hasc_2 = models.CharField(max_length=15)
+    cc_2 = models.CharField(max_length=15)
+    type_2 = models.CharField(max_length=50)
+    engtype_2 = models.CharField(max_length=50)
+    validfr_2 = models.CharField(max_length=25)
+    validto_2 = models.CharField(max_length=25)
+    remarks_2 = models.CharField(max_length=100)
+    id_3 = models.IntegerField()
+    name_3 = models.CharField(max_length=75)
+    varname_3 = models.CharField(max_length=100)
+    nl_name_3 = models.CharField(max_length=75)
+    hasc_3 = models.CharField(max_length=25)
+    type_3 = models.CharField(max_length=50)
+    engtype_3 = models.CharField(max_length=50)
+    validfr_3 = models.CharField(max_length=25)
+    validto_3 = models.CharField(max_length=25)
+    remarks_3 = models.CharField(max_length=50)
+    id_4 = models.IntegerField()
+    name_4 = models.CharField(max_length=100)
+    varname_4 = models.CharField(max_length=100)
+    type_4 = models.CharField(max_length=25)
+    engtype_4 = models.CharField(max_length=25)
+    validfr_4 = models.CharField(max_length=25)
+    validto_4 = models.CharField(max_length=25)
+    remarks_4 = models.CharField(max_length=50)
+    id_5 = models.IntegerField()
+    name_5 = models.CharField(max_length=75)
+    type_5 = models.CharField(max_length=25)
+    engtype_5 = models.CharField(max_length=25)
+    shape_leng = models.DecimalField(max_digits=65535, decimal_places=65535)
+    shape_area = models.DecimalField(max_digits=65535, decimal_places=65535)
+    the_geom = models.MultiPolygonField()
+    objects = models.GeoManager()
+    class Meta:
+        db_table = u'haiti_admin'
--- damage/views.py
+++ damage/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from haiti.damage.models import DamageReports, HaitiAdmin
 
 def index(request): 
     return render_to_response("damage.html")
@@ -9,7 +10,13 @@ def atlas(request):
     return render_to_response("damage-atlas.html")
 
 def reports(request): 
-    return render_to_response("damage-reports.html")
+    reports = DamageReports.objects.all()
+    return render_to_response("damage-reports.html",
+                              {
+            "reports" :reports,
+            })
 
 def new_report(rquest):
-    return render_to_response("damage-reports-new.html")
+    departments = HaitiAdmin.objects.all()
+    return render_to_response("damage-reports-new.html", 
+                              {"departments" : departments})
